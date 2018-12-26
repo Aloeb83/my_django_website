552d28deb5c7e37b81d035cc1154ea42aec3e181
Alec Loeber <alecloeber@yahoo.com>
2018-12-26 14:22:27 -0500

    Add Feature to Comment on Post.

--- blog/urls.py
+++ blog/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
 
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name="add_comment_to_post"),
+
 ]
